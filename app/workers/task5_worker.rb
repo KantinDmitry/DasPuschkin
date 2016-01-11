6547d9d473991dc5bfb98872c941c1d77cf51f35
KantinDmitry <kantindm@gmail.com>
2016-01-11 12:43:06 +0300

    task 5 with 1 db request

--- app/workers/task5_worker.rb
+++ app/workers/task5_worker.rb
@@ -7,15 +7,20 @@ class Task5Worker
     # question = Regexp.escape question
     @token = token
     @task_id = task_id
-    request_array = replace_each_word Regexp.escape(question)
+    replasements = replace_each_word Regexp.escape(question)
+    replasements.map! { |str| "(#{str})"}
+    regexp = "(#{replasements.join('|')}){1}"
+    result = Verse.where("text ~ ?", regexp).limit(1)[0]
 
-    start = Time.now
-    # Thread.abort_on_exception = true
-    request_array.each do |request_pattern|
-      thread_function request_pattern, question, start
-      # Thread.new { thread_function request_pattern, question, start }
+    if result then
+      result = result.text.match(regexp.gsub('\w', '\S')).to_s
+      different_words = find_difference question, result
+      @answer =  different_words.join ','
+    else
+      @answer = 'not found'
     end
-    puts "QUIZ lvl 5. Q: #{question}"
+    server_answer = send_answer
+    puts "QUIZ lvl 5. Question: #{question.inspect};\tanswer: #{@answer};\tLine: #{result};  #{server_answer.class} - #{server_answer.body}"
   end
 
   def replace_each_word string
@@ -28,27 +33,26 @@ class Task5Worker
     replasements
   end
 
-  def thread_function request_pattern, question, start
-    request_result = Verse.where("text ~ ?", request_pattern).limit(1)[0]
-
-    if request_result
-      puts "Time: #{(Time.now - start)*1000}"
-      text = request_result.text.match(request_pattern.sub('\w', '\S')).to_s
-      start_index = request_pattern.gsub("\\",'').index '[w'
-      correct_word = text[start_index..-1].split[0].gsub(/[,\.]/,'')
-      incorrect_word = question[start_index..-1].split[0].gsub(/[,\.]/,'')
-      answer = "#{correct_word},#{incorrect_word}"
+  def find_difference question, line
+    @start_index = 0
+    line.length.times do |i|
+      if question[i] != line[i] then
+        @start_index = i
+        break
+      end
     end
+    correct_word = line[@start_index..-1].split[0].gsub(/[,\.!\?]/, '')
+    incorrect_word = question[@start_index..-1].split[0].gsub(/[,\.!\?]/, '')
+    [correct_word,incorrect_word]
+  end
 
-    if answer
-      uri = URI("http://pushkin.rubyroid.by/quiz")
-      answer_params = {
-        answer: answer,
-        token: @token,
-        task_id: @task_id
-      }
-      server_answer = Net::HTTP.post_form(uri, answer_params)
-      puts "QUIZ lvl 5. Question: #{question.inspect};\tanswer: #{answer};\tLine: #{text};  #{server_answer.class} - #{server_answer.body}"
-    end
+  def send_answer
+    uri = URI("http://pushkin.rubyroid.by/quiz")
+    answer_params = {
+      answer: @answer,
+      token: @token,
+      task_id: @task_id
+    }
+    server_answer = Net::HTTP.post_form(uri, answer_params)
   end
 end
